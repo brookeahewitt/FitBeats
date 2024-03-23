a33c323733dc4f40d8396ea3bdfb532765d0bb92
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2024-03-23 17:22:58 -0400

    Added google login

--- FitBeats/FitBeatsApp/apps.py
+++ FitBeats/FitBeatsApp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FitbeatsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FitBeatsApp'
+
--- FitBeats/FitBeatsApp/urls.py
+++ FitBeats/FitBeatsApp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('generate', views.generate, name="generate"),
+    #path('logout', LogoutView.as_view()),
+    path('accounts/', include('allauth.urls')),
+
 ]
