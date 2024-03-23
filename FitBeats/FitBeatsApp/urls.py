d81419f84ccb13745bf25799a8dee9ff1192484c
Hamoda434 <99446119+Hamoda434@users.noreply.github.com>
2024-03-23 16:16:16 -0400

    Created exercises.html

--- FitBeats/FitBeatsApp/urls.py
+++ FitBeats/FitBeatsApp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('exercises/', views.exercises, name='exercises')
+
 ]
--- FitBeats/FitBeatsApp/views.py
+++ FitBeats/FitBeatsApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def exercises(request):
+    return render(request, 'exercises.html')
